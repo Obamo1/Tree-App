ed20214379edb3ae2ce534e6b4195660c01fd8da
obamo1 <joobamo@gmail.com>
2022-01-05 12:50:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//Open your console and add the 1st code to change image
+
+
+//Open your console and add the 2nd code to change alt text
+
+// Then change the tree's image, shape and align text to center
+
+let myTree = document.querySelector('img, src');
+myTree.style.width = '150px'
+myTree.style.borderRadius ='50%'
+
+let textK = document.querySelector('h1');
+textK.style.textAlign = 'center'
